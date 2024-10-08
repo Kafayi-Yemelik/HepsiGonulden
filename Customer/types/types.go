c97d5c01bef92c1f621db9817788f01d2d0fb3b5
Gonul Su Turkekole <turkekolegonulsu@gmail.com>
2024-10-08 23:47:57 +0300

    put endpoint edit

--- Customer/types/types.go
+++ Customer/types/types.go
@@ -53,7 +53,7 @@ type CustomerResponseModel struct {
 type CustomerUpdateModel struct {
 	FirstName      string            `bson:"first_name" json:"first_name"`
 	LastName       string            `bson:"last_name" json:"last_name"`
-	Age            string            `bson:"age" json:"age"`
+	Age            int               `bson:"age" json:"age"`
 	Phone          string            `bson:"phone" json:"phone"`
 	Address        string            `bson:"address" json:"address"`
 	City           string            `bson:"city" json:"city"`
